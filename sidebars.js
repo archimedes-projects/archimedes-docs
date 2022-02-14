5d0958ac47c99f4ac6abf471bcd4227e76c0dc00
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2022-02-14 13:24:43 +0100

    docs: add migration guide page to ArchimedesJS

--- sidebars.js
+++ sidebars.js
@@ -1,4 +1,4 @@
 module.exports = {
-  js: ['js/introduction', 'js/installation', 'js/arch', 'js/utils', 'js/components'],
+  js: ['js/introduction', 'js/installation', 'js/arch', 'js/utils', 'js/components', 'js/migration_guide'],
   jvm: ['jvm/introduction']
 }
